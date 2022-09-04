ccd0bf65e0aefd2314c30bf22d8c0607951df10c
Yunus Emre Çınar <www.yunuscinar4141@gmail.com>
2022-09-04 20:03:18 +0300

    Advice Generator

---
+++ advice-generator-app-main/index.js
@@ -0,0 +1,25 @@
+const quote = document.querySelector("blockquote");
+const button = document.querySelector("button");
+const id = document.querySelector("section p span");
+const sect = document.querySelector("section");
+const oldQuote = quote.textContent;
+var newQuote;
+
+getQuote();
+
+function getQuote() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      quote.textContent = `"${data.slip.advice}"`;
+      id.textContent = data.slip.id;
+      console.log(data.slip.advice);
+    });
+
+  quote.classList.add("slide");
+  setTimeout(() => {
+    quote.classList.remove("slide");
+  }, 1280);
+}
+
+document.querySelector("button").addEventListener("click", getQuote);
